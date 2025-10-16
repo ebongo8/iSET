1ec692f61eeff9540d4233a3ba10f5c1707a1a3d
Erin Bongo <ebongo14@verizon.net>
2025-10-15 23:14:32 -0400

    Updated gitignore to ignore data folder in test_files. Added other test files.

---
+++ Handwritten Digit Classification/test_files/test_integration.py
@@ -0,0 +1,53 @@
+import torch
+import copy
+from test_files.utils import get_model, get_dataloaders, get_device_type
+from src.classifier_model import ImageClassifier
+
+
+def test_training_loop_integrity():
+    """
+    TC-IT-01 Training Loop Integrity: Capture model weights before and after a single training step (optimizer.step()).
+    Expected result: The weights after the step are different from the weights before the step.
+    # TODO test is failing. Need to fix.
+    """
+    device_type = get_device_type(windows_os=False)
+    device = torch.device(device_type)
+    model = get_model()
+    train_loader, _ = get_dataloaders()
+
+    optimizer = torch.optim.SGD(model.parameters(), lr=0.01)
+    criterion = torch.nn.CrossEntropyLoss()
+
+    data, target = next(iter(train_loader))
+    data, target = data.to(device), target.to(device)
+
+    before_weights = copy.deepcopy(next(model.parameters()).clone())
+    optimizer.zero_grad()
+    output = model(data)
+    loss = criterion(output, target)
+    loss.backward()
+    optimizer.step()
+    after_weights = next(model.parameters()).clone()
+
+    assert not torch.equal(before_weights, after_weights), "Weights should update after training step"
+
+
+def test_save_load_integrity(tmp_path):
+    """
+    TC-IT-02 Save/Load Integrity: Save a model's state_dict, load it into a new model instance, and compare the weights.
+
+    Expected results: The weights of the new model are identical to the weights of the original model.
+    """
+    model = get_model()
+    save_path = tmp_path / "model.pth"
+    torch.save(model.state_dict(), save_path)
+
+    device_type = get_device_type()
+    device = torch.device(device_type)
+    new_model = ImageClassifier().to(device)
+    new_model.load_state_dict(torch.load(save_path, map_location=device_type))
+
+    new_model.load_state_dict(torch.load(save_path))
+
+    for p1, p2 in zip(model.parameters(), new_model.parameters()):
+        assert torch.equal(p1, p2), "Weights must match after load"
--- Handwritten Digit Classification/test_files/test_unit.py
+++ Handwritten Digit Classification/test_files/test_unit.py
@@ -3,8 +3,14 @@ from test_files.utils import get_model, get_dataloaders, get_device_type
 
 
 def test_device_placement():
+    """
+    TC-UT-03 Device Placement Verification: Instantiate the model and move it to the target device.
+    Expected result: next(model.parameters()).device returns the correct device (e.g., 'cuda:0' or 'cpu').
+    """
     device_type = get_device_type()
     model = get_model()
     # Get the device of the first parameter of the model
     param_device = next(model.parameters()).device
     assert param_device.type == device_type
+
+
